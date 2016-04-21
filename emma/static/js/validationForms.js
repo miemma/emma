de821fb1a3daf4002244a0b424181334862574b1
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-04-21 15:36:10 -0500

    Agregadas validaciones al formulario de agendar una cita

---
+++ emma/static/js/validationForms.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+  $('#dateForm').validate({
+    rules: {
+      number: {
+        number: true,
+        maxlength: 10
+      }
+    }
+    
+  });
+
+  // Mensajes de error
+
+  jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio",
+    remote: "Please fix this field.",
+    email: "Ingresa una dirección de correo valida",
+    url: "Please enter a valid URL.",
+    date: "Please enter a valid date.",
+    dateISO: "Please enter a valid date (ISO).",
+    number: "Ingresa un numero valido",
+    digits: "Please enter only digits.",
+    creditcard: "Please enter a valid credit card number.",
+    equalTo: "Please enter the same value again.",
+    accept: "Please enter a value with a valid extension.",
+    maxlength: jQuery.validator.format("No ingreses mas de {0} caracteres."),
+    minlength: jQuery.validator.format("Ingresa al menos {0} caracter."),
+    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+  });
+  
+});
