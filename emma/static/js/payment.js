f0a7636b74c2c1fd270b09e65b7ef93eb1ad8ff7
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-05 14:55:22 -0500

    Refactorizado el script y encontrado error de margen al final

--- emma/static/js/payment.js
+++ emma/static/js/payment.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   OpenPay.setId('mg0kzdwsiduimlfaudun');
   OpenPay.setApiKey('pk_106c640a77f94dd49bae11cf94937075');
   OpenPay.setSandboxMode(true);
-	var deviceSessionId = OpenPay.deviceData.setup("addcard-form", "devsessionid");
-});
+	OpenPay.deviceData.setup("addcard-form", "devsessionid");
 
 $('#pay-button').on('click', function(event) {
   event.preventDefault();
@@ -17,13 +16,9 @@ $('#pay-button').on('click', function(event) {
 		var token_id = response.data.id;
 		$('#token_id').val(token_id);
 		$('#addcard-form').submit();
-		console.log("Bien Hecho");
 	};
 
 	var error_callbak = function(response) {
 		$('.addcard-alert').show();
-		$("#pay-button").prop("disabled", false);
-		var desc = response.data.description != undefined ? response.data.description : response.message;
-		console.log(desc);
-		console.log("Mal Hecho");
 	};
+});
