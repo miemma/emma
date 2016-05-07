b7a45a3b8f8062eeb66fe41d6678bf8d36f30dce
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-07 12:14:39 -0500

    Agregado loader al formulario de agregar tarjeta

--- emma/static/js/payment.js
+++ emma/static/js/payment.js
@@ -3,22 +3,39 @@ $(document).ready(function() {
   OpenPay.setApiKey('pk_106c640a77f94dd49bae11cf94937075');
   OpenPay.setSandboxMode(true);
 	OpenPay.deviceData.setup("addcard-form", "devsessionid");
-
-$('#pay-button').on('click', function(event) {
-  event.preventDefault();
-  $("#pay-button").prop("disabled", true);
-  OpenPay.token.extractFormAndCreate(
-    'addcard-form', success_callbak, error_callbak
-  );
 });
 
-	var success_callbak = function(response) {
-		var token_id = response.data.id;
-		$('#token_id').val(token_id);
-		$('#addcard-form').submit();
-	};
+var success_callbak = function(response) {
+	var token_id = response.data.id;
+	$('#token_id').val(token_id);
+	$('.addcard-alert').hide();
+	$('.addcard-loader').show();
+	$('#addcard-form').submit();
+};
 
-	var error_callbak = function(response) {
-		$('.addcard-alert').show();
-	};
+var error_callbak = function(response) {
+	$('.addcard-loader').hide();
+	$('.addcard-alert').show();
+};
+
+var $addcardForm = $('#addcard-form');
+$addcardForm.validate({
+	onkeyup: false,
+	onfocusout: false,
+	groups: {
+		security: "card_year card_month"
+	},
+	highlight: function(element, errorClass) {
+			$(element).removeClass(errorClass);
+	}
+});
+
+$('#pay-button').on('click', function(event) {
+  if ($addcardForm.valid()) {
+		$('.addcard-alert').hide();
+		$('.addcard-loader').show();
+		OpenPay.token.extractFormAndCreate(
+			'addcard-form', success_callbak, error_callbak
+		);
+	}
 });
