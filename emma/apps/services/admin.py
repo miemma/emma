c6a2197c048c8c10fc317df46e1b59b8ef380a2b
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-11 18:19:25 -0500

    Agregada funcion de export al modulo de services

--- emma/apps/services/admin.py
+++ emma/apps/services/admin.py
@@ -3,19 +3,25 @@
 
 from django.contrib import admin
 
+from emma.core.utils import export_as_xls
 from . import models
 
 
 @admin.register(models.Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
 
 
 @admin.register(models.Workshop)
 class WorkshopAdmin(admin.ModelAdmin):
     list_display = ('service', 'name')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
 
 
 @admin.register(models.HiredService)
 class HiredServiceAdmin(admin.ModelAdmin):
-    pass
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
