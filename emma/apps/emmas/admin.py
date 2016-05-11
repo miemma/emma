c129a2f309798b1351281cf97b1c34b2d0f501d3
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-11 18:14:25 -0500

    Agregada funcion de export al modulo de emmas

--- emma/apps/emmas/admin.py
+++ emma/apps/emmas/admin.py
@@ -3,12 +3,15 @@
 
 from django.contrib import admin
 
+from emma.core.utils import export_as_xls
 from . import models
 
 
 @admin.register(models.Emma)
 class EmmaAdmin(admin.ModelAdmin):
     list_display = ('get_full_name', 'movile_phone', 'phone')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
 
     def get_full_name(self, obj):
         return obj.user.get_full_name()
