3eeea4e6f385c7d0bf21b661fa092718fce672d9
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-11 18:11:38 -0500

    Agregada funcio de exportar al modulo de customers

--- emma/core/utils.py
+++ emma/core/utils.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
 from django.template import loader
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+
+from pyExcelerator import *
 
 from random import choice
 from string import ascii_lowercase, digits
@@ -40,3 +44,38 @@ def generate_random_username(length=16, chars=ascii_lowercase+digits,
         )
     except User.DoesNotExist:
         return username
+
+
+def export_as_xls(modeladmin, request, queryset):
+    """
+    Generic xls export admin action.
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = modeladmin.model._meta
+
+    wb = Workbook()
+    ws0 = wb.add_sheet('0')
+    col = 0
+    field_names = []
+    # write header row
+    for field in opts.fields:
+        ws0.write(0, col, field.name)
+        field_names.append(field.name)
+        col = col + 1
+
+    row = 1
+    # Write data rows
+    for obj in queryset:
+        col = 0
+        for field in field_names:
+            val = unicode(getattr(obj, field)).strip()
+            ws0.write(row, col, val)
+            col = col + 1
+        row = row + 1
+
+    wb.save('/Users/mauriciodinki/Proyectos/emma/emma/emma/media/output.xls')
+    response = HttpResponse(open('/Users/mauriciodinki/Proyectos/emma/emma/emma/media/output.xls','r').read(),
+                            content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=%s.xls' % unicode(opts).replace('.', '_')
+    return response
