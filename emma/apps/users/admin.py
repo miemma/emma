7303a8ee46576e43d76698f1cfd0b62a5a133352
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-11 23:15:38 -0500

    Ultimos cambios en al interfas

--- emma/apps/users/admin.py
+++ emma/apps/users/admin.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from emma.core.utils import export_as_xls
@@ -9,21 +10,17 @@ from . import models
 
 
 def user_unicode(self):
-    return '%s %s' % (self.first_name, self.last_name)
+    return  u'%s, %s' % (self.last_name, self.first_name)
+
+
+class CustomUserAdmin(UserAdmin):
+    # as an example, this custom user admin orders users by email address
+    ordering = ('-date_joined',)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
 
 User.__unicode__ = user_unicode
-
 admin.site.unregister(User)
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'full_name', 'is_active', 'is_staff')
-    ordering = ('-date_joined',)
-
-    def full_name(self, obj):
-        return '%s %s' % (obj.first_name, obj.last_name)
-
+admin.site.register(User, CustomUserAdmin)
 
 @admin.register(models.Address)
 class AddresseAdmin(admin.ModelAdmin):
@@ -33,6 +30,11 @@ class AddresseAdmin(admin.ModelAdmin):
 
 @admin.register(models.Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('user', 'active_client', 'change_password')
+    list_display = ('user', 'get_email', 'active_client', 'change_password')
     actions = [export_as_xls]
     export_as_xls.short_description = "Export selected objects to XLS"
+
+    def get_email(self, obj):
+        return obj.user.email
+
+    get_email.short_description = 'Email'
