ff4a9a2781e28bdd2361abdb55c2ee5f79320ee0
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-11 19:15:25 -0500

    Agregada funcion de export al modulo de adultos

--- emma/apps/adults/admin.py
+++ emma/apps/adults/admin.py
@@ -3,9 +3,12 @@
 
 from django.contrib import admin
 
+from emma.core.utils import export_as_xls
 from . import models
 
 
 @admin.register(models.Adult)
 class AdultAdmin(admin.ModelAdmin):
     list_display = ('name', 'last_name', 'responsable')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
