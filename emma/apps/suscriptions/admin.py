2e29a35e8f2e396dc907d4bec35880fd66e5c79a
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-11 19:08:51 -0500

    Agregada funcion de export en el modulo de suscriptions

--- emma/apps/suscriptions/admin.py
+++ emma/apps/suscriptions/admin.py
@@ -3,19 +3,26 @@
 
 from django.contrib import admin
 
+from emma.core.utils import export_as_xls
 from . import models
 
 
 @admin.register(models.Suscription)
 class SuscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'id_customer', 'status', 'active')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
 
 
 @admin.register(models.History)
 class HistoryAdmin(admin.ModelAdmin):
     list_display = ('suscription', 'date', 'movement')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
 
 
 @admin.register(models.Charge)
 class ChargeAdmin(admin.ModelAdmin):
     list_display = ('suscription', 'date', 'amount', 'status')
+    actions = [export_as_xls]
+    export_as_xls.short_description = "Export selected objects to XLS"
