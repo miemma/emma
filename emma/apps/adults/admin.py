e8b050e64c81bd63c87977596cc72137cda685c8
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-05-14 01:12:13 -0500

    Refactor del modulo de adultos

--- emma/apps/adults/admin.py
+++ emma/apps/adults/admin.py
@@ -4,11 +4,12 @@
 from django.contrib import admin
 
 from emma.core.utils import export_as_xls
+
 from . import models
 
 
 @admin.register(models.Adult)
 class AdultAdmin(admin.ModelAdmin):
-    list_display = ('name', 'last_name', 'responsable')
+    list_display = ('first_name', 'last_name', 'responsable')
     actions = [export_as_xls]
     export_as_xls.short_description = "Export selected objects to XLS"
--- emma/apps/adults/models.py
+++ emma/apps/adults/models.py
@@ -2,43 +2,65 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from emma.apps.clients.models import Client
 from emma.apps.users.models import Address
 
 
+MARITAL_CHOICES = (
+    ('married', _('Married')),
+    ('widowed', _('Widowed')),
+    ('divorced', _('Divorced')),
+    ('separated', _('Separated')),
+    ('single', _('Single')),
+)
+
+
 class Adult(models.Model):
-    name = models.CharField(
+    first_name = models.CharField(
+        _('First name'),
         max_length=25,
         blank=False,
-        null=False
+        null=False,
     )
     last_name = models.CharField(
+        _('Last name'),
         max_length=25,
         blank=False,
-        null=False
+        null=False,
     )
     birthday = models.DateField(
+        _('Birthday'),
         blank=False,
-        null=False
+        null=False,
     )
     marital_status = models.CharField(
+        _('Marital status'),
         max_length=25,
         blank=False,
-        null=False
+        null=False,
+        choices=MARITAL_CHOICES
     )
     address = models.ForeignKey(
         Address,
+        verbose_name=_('Address'),
         blank=True,
         null=True,
 
     )
-    responsable = models.ForeignKey(Client)
-    is_candidate = models.BooleanField(default=False)
+    responsable = models.ForeignKey(
+        Client,
+        verbose_name=_('Responsable'),
+    )
+    is_candidate = models.BooleanField(
+        _('Candidate'),
+        default=False,
+    )
 
     class Meta:
-        verbose_name = 'Adult'
-        verbose_name_plural = 'Adults'
+        verbose_name = _('Adult')
+        verbose_name_plural = _('Adults')
 
     def __unicode__(self):
-        return ('%s %s') % (self.name, self.last_name)
+        return '%s %s' % (self.first_name, self.last_name)
