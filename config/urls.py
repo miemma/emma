5932beaaf7d46ba6dc2ace6803ae5c06d3903588
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-06-28 14:59:18 -0500

    Add adults module urls to url manager

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ from emma.apps.users import urls as users_urls
 from emma.apps.xauth import urls as xauth_urls
 from emma.apps.services import urls as services_urls
 from emma.apps.suscriptions import urls as suscription_urls
+from emma.apps.adults import urls as adult_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     url(r'', include(xauth_urls, namespace='xauth')),
     url(r'', include(services_urls, namespace='services')),
     url(r'', include(suscription_urls, namespace='suscriptions')),
+    url(r'', include(adult_urls, namespace='adult_urls')),
 ]
 
 if settings.DEBUG:
