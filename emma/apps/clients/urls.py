fdfdc6f2e69b346c306b235d2ac2abc3d2f8bfbc
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-06-29 12:06:44 -0500

    Add url to dashboard add card view

--- emma/apps/clients/urls.py
+++ emma/apps/clients/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
         view=views.ClientDetailView.as_view(),
         name='dashboard_client_detail'),
 
+    url(regex='^dashboard/tarjeta/$',
+        view=views.AddCardView.as_view(),
+        name='dashboard_add_card'),
 ]
