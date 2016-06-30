65d7c873426e42484d4a973436bf3e2a2a23b1ce
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-06-29 20:12:16 -0500

    Add logic to view emma information

--- emma/apps/emmas/views.py
+++ emma/apps/emmas/views.py
@@ -3,9 +3,19 @@
 from django.template.response import TemplateResponse
 from django.views.generic import View
 
+from emma.apps.adults.models import Adult
+from emma.apps.services.models import HiredService
+from emma.core.mixins import ClientRequiredMixin
 
-class MainEmma(View):
+
+class MainEmma(ClientRequiredMixin, View):
     template_name = 'emmas/emma_detail.html'
 
     def get(self, request):
-        return TemplateResponse(request, self.template_name)
+        adult = Adult.objects.get(responsable=request.user.client)
+        service = HiredService.objects.get(adult=adult)
+        emma = service.emma_assigned
+        ctx = {
+            'emma': emma
+        }
+        return TemplateResponse(request, self.template_name, ctx)
