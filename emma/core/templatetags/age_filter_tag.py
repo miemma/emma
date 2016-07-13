26fa664842c156b917cdd8dfff47c9cdcf42e246
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-13 17:28:23 -0500

    Refactor template tag

--- emma/core/templatetags/age_filter_tag.py
+++ emma/core/templatetags/age_filter_tag.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from django import template
+
 import datetime
 
 register = template.Library()
 
 @register.filter
-def age(bday, d=None):
-    print(bday)
-    if d is None:
-        d = datetime.date.today()
+def age(bdays):
+    if not isinstance(bdays, datetime.date):
+        bday = datetime.datetime.strptime(bdays, "%Y-%m-%d")
+    else:
+        bday = bdays
+    d = datetime.date.today()
     return (d.year - bday.year) - int((d.month, d.day) < (bday.month, bday.day))
