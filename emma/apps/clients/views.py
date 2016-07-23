2235a0c7dab774234a75666931758e5de69caa75
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-23 12:23:22 -0500

    Add email notification on changed user information

--- emma/apps/clients/views.py
+++ emma/apps/clients/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.shortcuts import redirect, get_object_or_404
 from django.template.response import TemplateResponse
 from django.views.generic import View
+from django.conf import settings
 
 from emma.apps.adults.models import Adult
 from emma.apps.clients.forms import UserInformationForm
@@ -15,6 +16,8 @@ from emma.core.mixins import ClientRequiredMixin, LoginRequiredMixin, \
     GetAdultMixin
 
 import openpay
+
+from emma.core.utils import send_email
 
 
 class ClientDetailView(GetAdultMixin, ClientRequiredMixin, View):
@@ -35,6 +38,14 @@ class ClientDetailView(GetAdultMixin, ClientRequiredMixin, View):
             user_form = UserInformationForm(request.POST, **kwargs)
             if user_form.is_valid():
                 user_form.save()
+                send_email(
+                    subject='email/subjects/notification_edit_profile.txt',
+                    body='email/notification_edit_profile.html',
+                    context={
+                        'full_name': request.user.get_full_name,
+                    },
+                    to_email=[settings.DEFAULT_EMAIL_TO],
+                )
                 ctx = {
                     'password_form': UpdatePasswordForm(),
                     'user_form': self.get_user_initial_form(request),
@@ -45,6 +56,7 @@ class ClientDetailView(GetAdultMixin, ClientRequiredMixin, View):
                     'password_form': UpdatePasswordForm(),
                     'user_form': user_form,
                 }
+
             return TemplateResponse(request, self.template_name, ctx)
         elif 'user_password' in request.POST:
             password_form = UpdatePasswordForm(request.POST, **kwargs)
