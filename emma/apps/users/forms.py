dc79eb3529edd1f1055cfd7b3cdfe243eb9c7399
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-08-24 11:22:15 -0500

    Updated admin to change password of the users

--- emma/apps/users/forms.py
+++ emma/apps/users/forms.py
@@ -38,7 +38,14 @@ class UserCreationForm(forms.ModelForm):
 
 
 class UserChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
+    password = ReadOnlyPasswordHashField(
+        label = ("Password"),
+        help_text = (
+            "Raw passwords are not stored, so there is no way to see "
+            "this user's password, but you can change the password "
+            "using <a href=\'../password/\'>this form</a>."
+        )
+    )
 
     class Meta:
         model = CoolUser
