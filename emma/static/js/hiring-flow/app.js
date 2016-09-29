a3426fe66bbaa0ee8810d641e69a5a74f7a0067d
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-28 23:21:56 -0500

    Fixed minor bug in plan detail controller. Created filters for labels.

--- emma/static/js/hiring-flow/app.js
+++ emma/static/js/hiring-flow/app.js
@@ -2,4 +2,58 @@ angular.module('emmaHiringFlow', [])
   .config(function($interpolateProvider) {
       $interpolateProvider.startSymbol('{$');
       $interpolateProvider.endSymbol('$}');
-    });
+    })
+  .filter('timeFormat', function () {
+    return function (timeString) {
+      timeString = timeString || '';
+      var out = '',
+        hours = parseInt(timeString.split(':')[0]),
+        minutes = parseInt(timeString.split(':')[1]);
+
+      if (hours > 12) {
+        out = (hours - 12) + ':' + minutes;
+      } else if (timeString != '') {
+        out = timeString;
+      } else {
+        out = 'horario sin asignar'
+      }
+
+      if (hours >= 12 && hours != 24) {
+        out += ' PM';
+      } else if (timeString != '') {
+        out += ' AM';
+      }
+
+      return out;
+    };
+  })
+  .filter('pluralize', function () {
+    return function (content, type, dependsOn, pluralLetter) {
+      content = content || null;
+      type = type || '';
+      pluralLetter = pluralLetter || '';
+      dependsOn = dependsOn || null;
+      var number = typeof content == Number ? content : 0,
+        singularText = '',
+        pluralText = '';
+      if (type == 'time') {
+        singularText = 'hora';
+        pluralText = 'horas';
+      } else if (type == 'session') {
+        singularText = 'sesión';
+        pluralText = 'sesiones';
+      } else if (type == '') {
+        singularText = content;
+        pluralText = content + pluralLetter;
+      }
+      if (number == 1) {
+        return number + ' ' + singularText;
+      } else if (pluralLetter == '') {
+        return number + ' ' + pluralText;
+      } else if (dependsOn == 1) {
+        return singularText;
+      } else {
+        return pluralText;
+      }
+    };
+  });
--- emma/static/js/hiring-flow/plan-detail.js
+++ emma/static/js/hiring-flow/plan-detail.js
@@ -1,229 +1,235 @@
 angular.module('emmaHiringFlow')
-  .controller('planDetailController', function ($scope, $filter) {
-    $scope.plan = {
-      allowsWorkshops: false,
-      allowsActivities: false,
-      workshops: [],
-      activities: [],
-      maxMonthlyHours: 0,
-      maxWeeklySessions: 0,
-      sessionNumbers: []
-    };
+  .controller('planDetailController', [
+    '$scope', 
+    '$filter', 
+    'timeFormatFilter', 
+    'pluralizeFilter',
+    function ($scope, $filter, timeFormat, pluralize) {
+      $scope.plan = {
+        allowsWorkshops: false,
+        allowsActivities: false,
+        workshops: [],
+        activities: [],
+        maxMonthlyHours: 0,
+        maxWeeklySessions: 0,
+        sessionNumbers: []
+      };
 
-    $scope.mergedLists = [];
+      $scope.mergedLists = [];
 
-    $scope.schedules = [
-      {
-        htmlValue: '9:00',
-        text: '9:00 AM'
-      },
-      {
-        htmlValue: '10:00',
-        text: '10:00 AM'
-      },
-      {
-        htmlValue: '11:00',
-        text: '11:00 AM'
-      },
-      {
-        htmlValue: '12:00',
-        text: '12:00 PM'
-      },
-      {
-        htmlValue: '13:00',
-        text: '1:00 PM'
-      },
-      {
-        htmlValue: '14:00',
-        text: '2:00 PM'
-      },
-      {
-        htmlValue: '15:00',
-        text: '3:00 PM'
-      },
-      {
-        htmlValue: '16:00',
-        text: '4:00 PM'
-      },
-      {
-        htmlValue: '17:00',
-        text: '5:00 PM'
-      },
-      {
-        htmlValue: '18:00',
-        text: '6:00 PM'
-      },
-      {
-        htmlValue: '19:00',
-        text: '7:00 PM'
-      },
-      {
-        htmlValue: '20:00',
-        text: '8:00 PM'
-      }
-    ];
-
-    $scope.sessions = [
-      {
-        dayName: 'Lunes',
-        htmlValue: 'monday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      },
-      {
-        dayName: 'Martes',
-        htmlValue: 'tuesday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      },
-      {
-        dayName: 'Miércoles',
-        htmlValue: 'wednesday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      },
-      {
-        dayName: 'Jueves',
-        htmlValue: 'thursday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      },
-      {
-        dayName: 'Viernes',
-        htmlValue: 'friday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      },
-      {
-        dayName: 'Sábado',
-        htmlValue: 'saturday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      },
-      {
-        dayName: 'Domingo',
-        htmlValue: 'sunday',
-        selected: false,
-        disabled: false,
-        initialHour: '',
-        duration: 0,
-        selectedWorkshop: ''
-      }
-    ];
-
-    $scope.usedHours = 0;
-
-    $scope.init = function (allowsWorkshops, allowsActivities, maxMonthlyHours, maxWeeklySessions) {
-      $scope.plan.allowsWorkshops = allowsWorkshops;
-      $scope.plan.allowsActivities = allowsActivities;
-      $scope.plan.maxMonthlyHours = maxMonthlyHours;
-      $scope.plan.maxWeeklySessions = maxWeeklySessions;
-      $scope.generateLists();
-    };
-
-    $scope.generateLists = function () {
-      var i;
-      if ($scope.plan.allowsWorkshops) {
-        angular.forEach(angular.element('[data-workshop-name]'), function (elem) {
-          $scope.plan.workshops.push({
-            id: $scope.plan.workshops.length + 1,
-            name: angular.element(elem).attr('data-workshop-name'),
-            isSelected: false
-          });
-        });
-      }
-      if ($scope.plan.allowsActivities) {
-        angular.forEach(angular.element('[data-activity-name]'), function (elem) {
-          $scope.plan.activities.push({
-            id: $scope.plan.activities.length + 1,
-            name: angular.element(elem).attr('data-activity-name'),
-            isSelected: false
-          });
-        });
-      }
-      for (i = 1; i <= $scope.plan.maxWeeklySessions; i++) {
-        $scope.plan.sessionNumbers.push({
-          value: i,
-          text: i + (i == 1 ? ' hora' : ' horas')
-        });
-      }
-    };
-
-    $scope.getHoursLeft = function () {
-      var selectedDays = $filter('filter')($scope.sessions, {selected: true});
-      $scope.usedHours = 0;
-      angular.forEach(selectedDays, function(element) {
-        $scope.usedHours += element.duration;
-      });
-      return ($scope.plan.maxMonthlyHours / 4) - $scope.usedHours;
-    };
-
-    $scope.mergeLists = function () {
-      $scope.mergedLists = $filter('filter')($scope.plan.workshops.concat($scope.plan.activities),
-          {isSelected: true});
-    };
-
-    $scope.disableDays = function (day) {
-      var selectedDays = $filter('filter')($scope.sessions, {selected: true});
-      var totalHours = 0;
-      var validDuration = $scope.getHoursLeft() >= 0;
-      var checkDays = true;
-      angular.forEach(selectedDays, function (element) {
-        if ((element.duration == 0 || element.duration == null) 
-            && day !== element) {
-          checkDays = false;
+      $scope.schedules = [
+        {
+          htmlValue: '9:00',
+          text: '9:00 AM'
+        },
+        {
+          htmlValue: '10:00',
+          text: '10:00 AM'
+        },
+        {
+          htmlValue: '11:00',
+          text: '11:00 AM'
+        },
+        {
+          htmlValue: '12:00',
+          text: '12:00 PM'
+        },
+        {
+          htmlValue: '13:00',
+          text: '1:00 PM'
+        },
+        {
+          htmlValue: '14:00',
+          text: '2:00 PM'
+        },
+        {
+          htmlValue: '15:00',
+          text: '3:00 PM'
+        },
+        {
+          htmlValue: '16:00',
+          text: '4:00 PM'
+        },
+        {
+          htmlValue: '17:00',
+          text: '5:00 PM'
+        },
+        {
+          htmlValue: '18:00',
+          text: '6:00 PM'
+        },
+        {
+          htmlValue: '19:00',
+          text: '7:00 PM'
+        },
+        {
+          htmlValue: '20:00',
+          text: '8:00 PM'
         }
-      });
-      if (checkDays && validDuration) {
-        if (selectedDays.length >= $scope.plan.maxWeeklySessions) {
-          $scope.sessions = angular.forEach($scope.sessions, function (element) {
-            if (!element.selected) {
-              element.disabled = true;
-            }
+      ];
+
+      $scope.sessions = [
+        {
+          dayName: 'Lunes',
+          htmlValue: 'monday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        },
+        {
+          dayName: 'Martes',
+          htmlValue: 'tuesday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        },
+        {
+          dayName: 'Miércoles',
+          htmlValue: 'wednesday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        },
+        {
+          dayName: 'Jueves',
+          htmlValue: 'thursday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        },
+        {
+          dayName: 'Viernes',
+          htmlValue: 'friday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        },
+        {
+          dayName: 'Sábado',
+          htmlValue: 'saturday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        },
+        {
+          dayName: 'Domingo',
+          htmlValue: 'sunday',
+          selected: false,
+          disabled: false,
+          initialHour: '',
+          duration: 0,
+          selectedWorkshop: ''
+        }
+      ];
+
+      $scope.usedHours = 0;
+
+      $scope.init = function (allowsWorkshops, allowsActivities, maxMonthlyHours, maxWeeklySessions) {
+        $scope.plan.allowsWorkshops = allowsWorkshops;
+        $scope.plan.allowsActivities = allowsActivities;
+        $scope.plan.maxMonthlyHours = maxMonthlyHours;
+        $scope.plan.maxWeeklySessions = maxWeeklySessions;
+        $scope.generateLists();
+      };
+
+      $scope.generateLists = function () {
+        var i;
+        if ($scope.plan.allowsWorkshops) {
+          angular.forEach(angular.element('[data-workshop-name]'), function (elem) {
+            $scope.plan.workshops.push({
+              id: $scope.plan.workshops.length + 1,
+              name: angular.element(elem).attr('data-workshop-name'),
+              isSelected: false
+            });
           });
-        } else {
-          angular.forEach(selectedDays, function (element) {
-            totalHours += element.duration;
+        }
+        if ($scope.plan.allowsActivities) {
+          angular.forEach(angular.element('[data-activity-name]'), function (elem) {
+            $scope.plan.activities.push({
+              id: $scope.plan.activities.length + 1,
+              name: angular.element(elem).attr('data-activity-name'),
+              isSelected: false
+            });
           });
-          if (totalHours * 4 >= $scope.plan.maxMonthlyHours) {
+        }
+        for (i = 1; i <= $scope.plan.maxWeeklySessions; i++) {
+          $scope.plan.sessionNumbers.push({
+            value: i,
+            text: i + (i == 1 ? ' hora' : ' horas')
+          });
+        }
+      };
+
+      $scope.getHoursLeft = function () {
+        var selectedDays = $filter('filter')($scope.sessions, {selected: true});
+        $scope.usedHours = 0;
+        angular.forEach(selectedDays, function(element) {
+          $scope.usedHours += element.duration;
+        });
+        return parseInt(($scope.plan.maxMonthlyHours / 4) - $scope.usedHours);
+      };
+
+      $scope.mergeLists = function () {
+        $scope.mergedLists = $filter('filter')($scope.plan.workshops.concat($scope.plan.activities),
+            {isSelected: true});
+      };
+
+      $scope.disableDays = function (day) {
+        var selectedDays = $filter('filter')($scope.sessions, {selected: true});
+        var totalHours = 0;
+        var validDuration = $scope.getHoursLeft() >= 0;
+        var checkDays = true;
+        angular.forEach(selectedDays, function (element) {
+          if ((element.duration == 0 || element.duration == null) 
+              && day !== element) {
+            checkDays = false;
+          }
+        });
+        if (checkDays && validDuration) {
+          if (selectedDays.length >= $scope.plan.maxWeeklySessions) {
             $scope.sessions = angular.forEach($scope.sessions, function (element) {
               if (!element.selected) {
                 element.disabled = true;
               }
             });
           } else {
-            $scope.sessions = angular.forEach($scope.sessions, function (element) {
-              element.disabled = false;
+            angular.forEach(selectedDays, function (element) {
+              totalHours += element.duration;
             });
+            if (totalHours - parseInt($scope.plan.maxMonthlyHours / 4) == 0) {
+              $scope.sessions = angular.forEach($scope.sessions, function (element) {
+                if (!element.selected) {
+                  element.disabled = true;
+                }
+              });
+            } else {
+              $scope.sessions = angular.forEach($scope.sessions, function (element) {
+                element.disabled = false;
+              });
+            }
+          }
+        } else {
+          if (!validDuration) {
+            console.log('Parece que te excederas de las horas permitidas en tu plan.');
+            day.duration = 0;
+          }
+          if (!checkDays) {
+            console.log('Antes de elegir un nuevo día, completa la información del anterior!');
+            day.selected = false;
           }
         }
-      } else {
-        if (!validDuration) {
-          console.log('Parece que te excederas de las horas permitidas en tu plan.');
-          day.duration = 0;
-        }
-        if (!checkDays) {
-          console.log('Antes de elegir un nuevo día, completa la información del anterior!');
-          day.selected = false;
-        }
-      }
-    };
-  });
+      };
+    }
+  ]);
