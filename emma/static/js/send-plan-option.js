0af655dfaedc65dc6a60bf4f74480c32988f9a57
Alan Sanchez <sanchezpineda03@gmail.com>
2016-10-05 23:15:53 -0500

    Added form loader in choose plan section.

--- emma/static/js/send-plan-option.js
+++ emma/static/js/send-plan-option.js
@@ -2,6 +2,8 @@ $('#select-plan-form button[type="submit"]').click(function (e) {
   e.preventDefault();
   var form = $('#select-plan-form')[0],
     planType = $('#select-plan-form input[name="plan"]');
+  $(form).find('button[type="submit"]').attr('disabled', 'disabled');
   planType.val($(this).attr('data-plan'));
+  $(form).siblings('.form-loader').show();
   form.submit();
 });
